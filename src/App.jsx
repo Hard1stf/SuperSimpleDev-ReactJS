a84c9604fe4e90951e385b68e4615d29782b2d19
Hard1stf <hardikvijeta2@gmail.com>
2026-04-14 18:50:17 +0530

    feat(TrackingPage.jsx): Move Tracking page to React and use Link

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePages';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 import './App.css'
 
@@ -12,6 +13,7 @@ const App = () => {
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage/>} />
       <Route path='orders' element={<OrdersPage />}/>
+      <Route path='tracking' element={<TrackingPage />}/>
     </Routes>
     </>
   );
