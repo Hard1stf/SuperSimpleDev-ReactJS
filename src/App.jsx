7235b143086b953e4fdb28541ba78b754fdce821
Hard1stf <hardikvijeta2@gmail.com>
2026-04-14 18:20:29 +0530

    feat(OrdersPage.jsx): Move orders page to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePages';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 
 import './App.css'
 
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage/>} />
+      <Route path='orders' element={<OrdersPage />}/>
     </Routes>
     </>
   );
