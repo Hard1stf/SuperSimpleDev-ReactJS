9a1d9f828ae1068ca296a47e0470e44e0c236d52
Hard1stf <hardikvijeta2@gmail.com>
2026-04-18 20:01:39 +0530

    update/fix(configure the base endpoint): Add server proxy config for /api and /images

--- Lesson-6-ecommerce-project/src/pages/checkout/HomePages.jsx
+++ Lesson-6-ecommerce-project/src/pages/checkout/HomePages.jsx
@@ -12,12 +12,12 @@ export const HomePage = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
     .then(res => {
       setProducts(res.data);
     });
 
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
     .then(res => {
       setCart(res.data);
     })
