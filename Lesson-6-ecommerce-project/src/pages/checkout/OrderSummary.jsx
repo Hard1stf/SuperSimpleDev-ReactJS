ee947540e24f69b4d0d78a387b78853d50050e93
Hard1stf <hardikvijeta2@gmail.com>
2026-04-20 17:37:54 +0530

    perf(CartItemDetail): Seprate cartitem details into a smaller component

--- Lesson-6-ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ Lesson-6-ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
-import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
+import { CartItemDetails } from './CartItemDetails';
 
 export const OrderSummary = ({ cart, deliveryOptions }) => {
   return (
@@ -23,35 +23,10 @@ export const OrderSummary = ({ cart, deliveryOptions }) => {
                   </div>
 
                   <div className="cart-item-details-grid">
-                    <img
-                      className="product-image"
-                      src={cartItem.product.image}
+                    <CartItemDetails cartItem={cartItem} />
+
+                    <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions}
                     />
-
-                    <div className="cart-item-details">
-                      <div className="product-name">
-                        {cartItem.product.name}
-                      </div>
-                      <div className="product-price">
-                        {formatMoney(cartItem.product.priceCents)}
-                      </div>
-                      <div className="product-quantity">
-                        <span>
-                          Quantity:{' '}
-                          <span className="quantity-label">
-                            {cartItem.quantity}
-                          </span>
-                        </span>
-                        <span className="update-quantity-link link-primary">
-                          Update
-                        </span>
-                        <span className="delete-quantity-link link-primary">
-                          Delete
-                        </span>
-                      </div>
-                    </div>
-
-                   <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions}/>
                   </div>
                 </div>
               </>
