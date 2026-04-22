de546523e25ca1ab8f01ec238a78627012fa4a00
Hard1stf <hardikvijeta2@gmail.com>
2026-04-22 20:05:03 +0530

    feat(delete cart items): Create delete cart item feature

--- Lesson-6-ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ Lesson-6-ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -19,7 +19,7 @@ export const OrderSummary = ({ cart, deliveryOptions, loadCart }) => {
                   <DeliveryDate selectedDeliveryOption={selectedDeliveryOption}/>
 
                   <div className="cart-item-details-grid">
-                    <CartItemDetails cartItem={cartItem} />
+                    <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
                     <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
                   </div>
