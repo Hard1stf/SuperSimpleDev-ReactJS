834920abe6a70c860bd91f73334040a7d3cc1f6a
Hard1stf <hardikvijeta2@gmail.com>
2026-04-25 15:24:50 +0530

    test(foramtMoney-Neg): Add a test for negative values.

--- Lesson-6-ecommerce-project/src/utils/money.js
+++ Lesson-6-ecommerce-project/src/utils/money.js
@@ -1,3 +1,8 @@
 export const formatMoney = (amountCents) => {
-    return `$${(amountCents / 100).toFixed(2)}`
+    if(amountCents < 0){
+        amountCents *= -1;
+        // console.log(amountCents);
+        return `-$${(amountCents / 100).toFixed(2)}`;
+    }
+    return `$${(amountCents / 100).toFixed(2)}`;
 } 
